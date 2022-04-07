5e364c50e9b5fb4d100af1fa0d3b4ea8df17b487
william walsh <william.walsh@digitary.net>
2022-04-07 11:45:45 +0100

    Anonymised name.

--- digital_business_card/src/components/Profile.js
+++ digital_business_card/src/components/Profile.js
@@ -4,9 +4,9 @@ const Profile = () => {
   return (
     <>
         <img className='profile--photo' src={profile} alt='Profile Photo' />
-        <div className='profile--full_name'>William Walsh</div>
+        <div className='profile--full_name'>John Smith</div>
         <div className='profile--title'>Fullstack Developer</div>
-        <div className='profile--website-name'>williamwalsh.web</div>
+        <div className='profile--website-name'>johnsmith.web</div>
         <div className='profile--button-container'>
             <button className="profile--email-button"><i class="fa fa-envelope">  Email</i></button>
             <button className="profile--linkedin-button"><i class="fa fa-linkedin">  LinkedIn</i></button>
